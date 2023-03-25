f3fb3a7a794043cacaf64ab1d1999be3d80e52d6
rajpatel8 <rajphx299@gmail.com>
2023-03-25 13:19:34 +0530

    Kommit

--- Scrap/1.hs
+++ Scrap/1.hs
@@ -32,7 +32,8 @@ reverse' xss = map reverse xss
 
 -- length function using map and sum
 length' :: [a] -> Int
-length' xs = sum (map (const 1) xs) -- const 1 is a function which always return 1 and map it to each element of list  
+length' xs = sum (map (const 1) xs)
+ -- const 1 is a function which always return 1 and map it to each element of list  
 
 -- Function to double each element of a list using map
 double :: [Int] -> [Int]
@@ -60,13 +61,63 @@ addup' ns = map (+1) (filter greaterOne ns)
     where 
         greaterOne n = n > 1
 
+-- length of list using map and sum
+length'' :: [a] -> Int
+length'' xs = sum (map (const 1) xs)
+
+
 -- take sentence as input and return length of each word in a list
 wordLengths :: String -> [Int]
 wordLengths sentence = map length (words sentence)
+
+-- multiply 3 nums
+mul :: Int -> (Int->(Int->(Int))) 
+mul x y z = x * y * z 
+
+respond :: String -> String
+respond phrase = 
+    if  elem '!' phrase
+        then "wow!"
+        else "uh.. ok"
+
+primetilln :: Int -> [Int]
+primetilln n = [x | x <- [2..n], isPrime x]
+    where
+        isPrime x = [y | y <- [2..x], x `mod` y == 0] == [x]
+
+-- coin toss function randomly return heads or tails on call using random function
+coinToss :: IO String
+coinToss = do
+    x <- randomRIO (0, 1) :: IO Int
+    if x == 0
+        then return "Heads"
+        else return "Tails"
+
+randomRIO :: (a0, b0) -> IO Int
+randomRIO (a, b) = do
+    gen <- getStdGen
+    let (value, newGen) = randomR a b gen
+    setStdGen newGen
+    return value
+
+setStdGen :: t0 -> IO a0
+setStdGen = undefined
+
+randomR :: a2 -> b0 -> a0 -> (a, b)
+randomR = undefined
+
+getStdGen :: IO a1
+getStdGen = getStdGen
+
+
 
 -- main function
 main = do
     -- print (sumEvenOdd [1..4] )
     -- print (length' [1,2,3])
     -- print (reverse' [[1,2,3], [4,5,6], [7,8,9]] )
-    print ( wordLengths "This is a sentence") 
+    -- print ( wordLengths "This is a sentence") 
+    print (mul 2 3 4)
+    -- print (respond "Hello!")
+    -- print(primetilln 10)
+    -- print(coinToss)
